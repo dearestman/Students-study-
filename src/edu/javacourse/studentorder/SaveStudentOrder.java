bf6042afee2af8fec64b99b42c1f39dd0929d41c
Stupakov Dmitriy <40610651+dearestman@users.noreply.github.com>
2020-08-25 17:40:22 +0300

    lesson 11. Get and Set

--- src/edu/javacourse/studentorder/SaveStudentOrder.java
+++ src/edu/javacourse/studentorder/SaveStudentOrder.java
@@ -6,23 +6,17 @@ import edu.javacourse.studentorder.domain.StudentOrder;
 public class SaveStudentOrder {
     public static void main(String[] args) {
         StudentOrder so = new StudentOrder();
-        so.hFirstName = "Алексей";
-        so.hFLastName = "Петров";
-        so.wFirstName = "Галина";
-        so.wFLastName = "Петровна";
-
-        StudentOrder s1 = new StudentOrder();
-        s1.hFirstName = "Алексей";
-        s1.hFLastName = "Петров";
-        s1.wFirstName = "Галина";
-        s1 .wFLastName = "Петровна";
+        so.sethFirstName("Алексей");
+        so.sethFLastName("Петров");
+        so.setwFirstName("Галина");
+        so.setwFLastName("Петровна");
 
         long ans = saveStudentOrder(so);
         System.out.println(ans);
     }
     static long saveStudentOrder(StudentOrder studentOrder) {
         long answer = 199;
-        System.out.println("saveStudentOrder: " + studentOrder.hFLastName);
+        System.out.println("saveStudentOrder: " + studentOrder.gethFLastName());
         return answer;
     }
 }
--- src/edu/javacourse/studentorder/domain/StudentOrder.java
+++ src/edu/javacourse/studentorder/domain/StudentOrder.java
@@ -1,8 +1,40 @@
 package edu.javacourse.studentorder.domain;
 
 public class StudentOrder {
-    public String hFirstName;
-    public String hFLastName;
-    public String wFirstName;
-    public String wFLastName;
+    private String hFirstName;
+    private String hFLastName;
+    private String wFirstName;
+    private String wFLastName;
+
+    public String gethFirstName() {
+        return hFirstName;
+    }
+
+    public void sethFirstName(String hFirstName) {
+        this.hFirstName = hFirstName;
+    }
+
+    public String gethFLastName() {
+        return hFLastName;
+    }
+
+    public void sethFLastName(String hFLastName) {
+        this.hFLastName = hFLastName;
+    }
+
+    public String getwFirstName() {
+        return wFirstName;
+    }
+
+    public void setwFirstName(String wFirstName) {
+        this.wFirstName = wFirstName;
+    }
+
+    public String getwFLastName() {
+        return wFLastName;
+    }
+
+    public void setwFLastName(String wFLastName) {
+        this.wFLastName = wFLastName;
+    }
 }
