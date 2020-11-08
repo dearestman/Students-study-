9affebbf21e3daf409c934f13e1f39594655296e
Дмитрий Ступаков <dmitrijstupakov@iMac-Dmitrij.local>
2020-11-08 20:44:52 +0300

    lesson 48. added insert child

--- src/edu/javacourse/studentorder/dao/StudentOrderDaoImpl.java
+++ src/edu/javacourse/studentorder/dao/StudentOrderDaoImpl.java
@@ -18,6 +18,15 @@ public class StudentOrderDaoImpl implements StudentOrderDao{
                      "certificate_id, register_office_id, marriage_date) " +
              "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
     // TODO refactoring- make one method
+
+    private static final String INSERT_CHILD = "INSERT INTO js_student_child(" +
+            "student_order_id, c_sur_name, c_given_name, " +
+            "c_patronymic, c_date_of_birth, c_certificate, c_certificate_date, " +
+            "c_register_office_id, c_post_index, c_street_code, c_building, c_extension, " +
+            "c_apartment)" +
+            "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);" ;
+
+
     private Connection getConnection() throws SQLException {
         Connection con = DriverManager.getConnection(
                 Config.getProperty(Config.DB_URL),
@@ -55,6 +64,7 @@ public class StudentOrderDaoImpl implements StudentOrderDao{
             }
             gkRs.close();
 
+            saveChildren(con,so, result);
 
         }
         catch (SQLException ex){
@@ -64,21 +74,51 @@ public class StudentOrderDaoImpl implements StudentOrderDao{
         return result;
     }
 
+    private void saveChildren(Connection con, StudentOrder so, Long soId) throws SQLException{
+
+        try(PreparedStatement stmt = con.prepareStatement(INSERT_CHILD)){
+            for (Child child : so.getChildren()){
+                stmt.setLong(1, soId);
+                setParamsForChild(stmt,child);
+                stmt.executeUpdate();
+            }
+        }
+
+    }
+
+    private void setParamsForChild(PreparedStatement stmt, Child child) throws SQLException {
+        setParamsForPerson(stmt, 2, child);
+        stmt.setString(6,child.getCertificateNumber());
+        stmt.setDate(7,Date.valueOf(child.getDateOfBirth()));
+        stmt.setLong(8,child.getIssueDepartment().getOfficeId());
+        setParamsForAddress(stmt,9, child);
+    }
+
     private void setParamsForAdult(PreparedStatement stmt, int start, Adult adult) throws SQLException {
-        stmt.setString(start,adult.getSurName());
-        stmt.setString(start+1,adult.getGivenName());
-        stmt.setString(start+2,adult.getPatronymic());
-        stmt.setDate(start+3, Date.valueOf(adult.getDateOfBirth()));
+        setParamsForPerson(stmt, start, adult);
         stmt.setString(start+4, adult.getPassportSerial());
         stmt.setString(start+5, adult.getPassportNumber());
         stmt.setDate(start+6, Date.valueOf(adult.getIssueDate()));
         stmt.setLong(start+7, adult.getIssueDepartment().getOfficeId());
         //husband address
-        Address h_address = adult.getAddress();
-        stmt.setString(start+8, h_address.getPostCode());
-        stmt.setLong(start+9, h_address.getStreet().getStreetCode());
-        stmt.setString(start+10, h_address.getBuilding());
-        stmt.setString(start+11, h_address.getExtension());
-        stmt.setString(start+12, h_address.getApartment());
+        setParamsForAddress(stmt, start + 8, adult);
+    }
+
+
+
+    private void setParamsForPerson(PreparedStatement stmt, int start, Person person) throws SQLException {
+        stmt.setString(start, person.getSurName());
+        stmt.setString(start +1, person.getGivenName());
+        stmt.setString(start +2, person.getPatronymic());
+        stmt.setDate(start +3, Date.valueOf(person.getDateOfBirth()));
+    }
+
+    private void setParamsForAddress(PreparedStatement stmt, int start, Person person) throws SQLException {
+        Address h_address = person.getAddress();
+        stmt.setString(start, h_address.getPostCode());
+        stmt.setLong(start +1, h_address.getStreet().getStreetCode());
+        stmt.setString(start +2, h_address.getBuilding());
+        stmt.setString(start +3, h_address.getExtension());
+        stmt.setString(start +4, h_address.getApartment());
     }
 }
