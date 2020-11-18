0d210ecb156b73c6f205e560a08dbbfcb6d5f4ac
Дмитрий Ступаков <dmitrijstupakov@iMac-Dmitrij.local>
2020-11-18 20:34:45 +0300

    lesson 57. More Inner join...

--- src/edu/javacourse/studentorder/dao/StudentOrderDaoImpl.java
+++ src/edu/javacourse/studentorder/dao/StudentOrderDaoImpl.java
@@ -29,10 +29,14 @@ public class StudentOrderDaoImpl implements StudentOrderDao{
             "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);" ;
 
 
-    private static final String SELECT_ORDERS = "SELECT ro.r_office_area_id, ro.r_office_name, so.* " +
+    private static final String SELECT_ORDERS = "SELECT ro.r_office_area_id, ro.r_office_name, so.*, " +
+            "po_h.P_office_area_id as h_p_office_area_id, po_h.p_office_name as h_p_office_name, " +
+            "po_w.P_office_area_id as w_p_office_area_id, po_w.p_office_name as w_p_office_name " +
             "FROM js_student_order so " +
             "INNER JOIN js_register_office ro ON ro.r_office_id = so.register_office_id " +
-            "WHERE student_order_status = 0 ORDER BY student_order_date ";
+            "INNER JOIN js_passport_office po_h ON po_h.p_office_id = so.h_passport_office_id " +
+            "INNER JOIN js_passport_office po_w ON po_w.p_office_id = so.w_passport_office_id " +
+            "WHERE student_order_status = 0 ORDER BY student_order_date";
 //    private static final String SELECT_ORDERS = "SELECT * FROM js_student_order WHERE student_order_status  = 0 ORDER BY student_order_date";
 
     private Connection getConnection() throws SQLException {
@@ -180,7 +184,10 @@ public class StudentOrderDaoImpl implements StudentOrderDao{
         adult.setPassportNumber(rs.getString(pref+"passport_number"));
         adult.setIssueDate(rs.getDate(pref+"passport_date").toLocalDate());
 
-        PassportOffice po = new PassportOffice(rs.getLong(pref+"passport_office_id"),"","");
+        Long poId = rs.getLong(pref + "passport_office_id");
+        String poArea = rs.getString(pref + "p_office_area_id");
+        String poName = rs.getString(pref + "p_office_name");
+        PassportOffice po = new PassportOffice(poId,poArea,poName);
         adult.setIssueDepartment(po);
 
         Address adr = new Address();
